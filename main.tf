c3628d6a47122773d585be39cfa229eaaf82af8b
vikash raj <rvvikashraj@gmail.com>
2025-11-29 19:24:10 +0530

    Update: Added CI/CD message and new output

--- main.tf
+++ main.tf
@@ -7,10 +7,15 @@ terraform {
 
 resource "local_file" "welcome" {
   filename = "${path.module}/welcome.txt"
-  content  = "Hello from Terraform! This file was created by Infrastructure as Code."
+  content  = "Hello from Terraform CI/CD! This pipeline runs automatically on every push!"
 }
 
 output "file_path" {
   value       = local_file.welcome.filename
   description = "Path to the created file"
+}
+
+output "file_content" {
+  value       = local_file.welcome.content
+  description = "Content of the file"
 }
